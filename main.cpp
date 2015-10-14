20b54c544fda4330cd3effd669f7ef82bcdf4a0d
Katanaco <jonathan.schmitt@colorado.edu>
2015-10-13 19:45:30 -0600

    added question loop to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,20 @@
 using namespace std;
 
 int main(){
-	cout << "hello world" << endl;
+	char ans;
+	string q[20] = {"alive","big","cute","furry","flying","warm_blooded","reptile","swim","tail","hibernate","poisonous","extinct","pet","mammal","carnivor","legs","scales","food","fictional","4legs"};
+	for (int i = 1; i <=20; i++){
+		cout << "question " << i << ": is it " << q[i-1] << "?" << endl;
+		cin >> ans;
+		if (ans == 'y'){
+			//add to animal traits
+		} else if (ans == 'n'){
+			//add to animal traits
+		} else{
+			cout << "invalid response" << endl;
+		}
+	}
+	cout << "it's a..." << endl;
+	//do some comparing
+	cout << "thing" << endl;
 }
